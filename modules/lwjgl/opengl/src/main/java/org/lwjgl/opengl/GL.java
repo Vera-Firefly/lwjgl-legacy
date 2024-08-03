319a72ba92be115f011ea671e7012e61814184c4
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-08-03 21:13:17 +0800

    再次修复

--- modules/lwjgl/opengl/src/main/java/org/lwjgl/opengl/GL.java
+++ modules/lwjgl/opengl/src/main/java/org/lwjgl/opengl/GL.java
@@ -304,7 +304,7 @@ public final class GL {
         if (Platform.get() == Platform.LINUX && renderer.startsWith("libOSMesa")) {
             if (System.getenv("POJAV_SPARE_FRAME_BUFFER") != null && System.getenv("POJAV_EXP_SETUP") != null) {
                 System.out.println("[LWJGL] You turned on the experimental settings and tried to use the frame buffer");
-                if (System.getenv("POJAV_ZINK_CRASH_HANDLE") != null || System.getenv("DCLAT_FRAMEBUFFER") != null) {
+                if (System.getenv("POJAV_SPARE_BRIDGE") != null || System.getenv("DCLAT_FRAMEBUFFER") != null) {
                     System.out.println("[LWJGL] Repair GL Context for Mesa renderer, use frame buffer");
                     long currentContext;
                     int[] dims = getNativeWidthHeight();
